c5396cea8c432209c1816bea81e0290cf4eb53cc
SkalT12 <68639273+SkalT12@users.noreply.github.com>
2020-08-20 19:35:26 +0400

    Door
    Add script

---
+++ Assets/OpenDoor.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenDoor : MonoBehaviour {
+
+	public float smooth = 2.0f;
+	public float DoorOpenAngle = 90.0f;
+
+	
+
+	private Vector3 defaultRot;
+	private Vector3 openRot;
+	private bool open;
+	private bool enter;
+
+	// Use this for initialization
+	void Start () {
+		
+			defaultRot = transform.eulerAngles;
+			openRot = new Vector3 (defaultRot.x, defaultRot.y + DoorOpenAngle, defaultRot.z);
+		}
+	
+	// Update is called once per frame
+	void Update () {
+		if (open) {
+			
+			transform.eulerAngles = Vector3.Slerp (transform.eulerAngles, openRot, Time.deltaTime * smooth);
+		} else {
+			
+			transform.eulerAngles = Vector3.Slerp (transform.eulerAngles, defaultRot, Time.deltaTime * smooth);
+
+		}
+		if (Input.GetKeyDown (KeyCode.E) && enter) {
+			open = !open;
+		}
+}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.tag == "Player") {
+			enter = true;
+			}
+		}
+
+    void OnTriggerExit(Collider col)
+{
+	if (col.tag == "Player") {
+		enter = false;
+	}
+}
+}
