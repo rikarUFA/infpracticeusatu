53335161b6b2593f540e895ccb494e54960dcc93
SkalT12 <68639273+SkalT12@users.noreply.github.com>
2020-11-24 22:16:35 +0500

    add minimap
    add minimap + sorting files

---
+++ Assets/Scripts/MiniMapScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapScript : MonoBehaviour
+{
+    public Transform PlayerTransf;
+    void LateUpdate()
+    {
+        UpdateCamPosition();
+    }
+    void UpdateCamPosition()
+    {
+        Vector3 newPos = PlayerTransf.position;
+        newPos.y = transform.position.y;
+        transform.position = newPos;
+
+        transform.rotation = Quaternion.Euler(90, PlayerTransf.eulerAngles.y, 0);
+
+    }
+}
